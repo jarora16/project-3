cdf09708888f35faa82876fded5856666a2cd32d
Jay Arora <arorajay640@gmail.com>
2020-12-09 12:50:15 -0500

    new design

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -5,17 +5,16 @@ const db = require("../models");
 
 mongoose.connect(
     process.env.MONGODB_URI ||
-    "mongodb://localhost/reactgooglebooks"
+    "mongodb://localhost/googleboks"
 );
 
 const bookSeed =
 {
-    authors: ["Sarah Schachner"],
-    description: "Call of Duty: Modern Warfare is a 2019 first-person shooter video game developed by Infinity Ward and published by Activision.",
-    rating: "4.4/5 Gamestop", 
+    authors: ["Suzanne Collins"],
+    description: "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
     image: "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
     link: "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
-    title: "Call of Duty: Modern Warfare",
+    title: "The Hunger Games",
 }
 
 
