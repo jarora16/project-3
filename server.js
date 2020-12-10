c1d8ee09d136c9ab4f55834b1d2f10d3e1eee895
Ali Mahmoud <961alimahmoud@gmail.com>
2020-12-10 08:48:39 -0500

    created new Google Strategy

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, () => {
+  console.log(accessToken);
+}));
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
