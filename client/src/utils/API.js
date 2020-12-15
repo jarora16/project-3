15992ec4324243583ff60ba37f19c1b3b9fc1200
Jay Arora <arorajay640@gmail.com>
2020-12-15 14:35:15 -0500

    restarting with base code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -26,7 +26,7 @@ var gamefunctions = {
   deleteGame: function (id) {
     return axios.delete("/api/games/" + id).then(Game => Game.data);
   },
-  // Saves a game to the database
+  // Saves a book to the database
    saveGame: function (gameData) {
     return axios.post("/api/games", gameData).then(Game => Game.data);
   },
--- routes/api/games.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const gamesController = require("../../controllers/gamesControllers");
-
-// Matches with "/api/games"
-router.route("/")
-  .post(gamesController.create);
-
-module.exports = router;
